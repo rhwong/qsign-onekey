b300eaa8dd11602daecd7c4771645a5b4a4b0515
贰狐RH <58497504+rhwong@users.noreply.github.com>
2023-07-02 05:23:34 +0800

    update auto ANDROID_ID

--- 启动qsign.bat
+++ 启动qsign.bat
@@ -1,2 +1,16 @@
+@echo off
 set JAVA_HOME=.\jre
-bin\unidbg-fetch-qsign --host=127.0.0.1 --port=13579  --count=1 --library=txlib/8.9.63 --android_id=此处替换你复制来的设备id
+set "textFile=device.json"
+set "ver=1.1.0"
+set "host=127.0.0.1"
+set "port=13579"
+set "count=1"
+set "library=txlib/"
+set "txlib_version=8.9.63"
+for /F "delims=" %%A in ('lib\jq.exe -r ".android_id" device.json') do set "android_id=%%A"
+echo Qsign API:http://%host%:%port%
+echo Qsign_version:%ver% TXlib_version:%txlib_version% Android_id=%android_id%
+
+
+bin\unidbg-fetch-qsign --host=%host% --port=%port%  --count=%count% --library=%library%%txlib_version% --android_id=%android_id%
+
