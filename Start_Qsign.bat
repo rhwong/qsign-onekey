f5a2920ab33cb7c1ad2408215a08ed2a51fb4ad7
贰狐RH <58497504+rhwong@users.noreply.github.com>
2023-07-07 10:53:36 +0800

    fix unix bug

--- Start_Qsign.bat
+++ Start_Qsign.bat
@@ -1,5 +1,5 @@
 @echo off
-
+setlocal enabledelayedexpansion
 set JAVA_HOME=.\jre
 set "ver=1.1.3"
 set "library=txlib/"
@@ -9,30 +9,38 @@ set "port=13579"
 set "key=1145141919810"
 set "json_file=%library%%txlib_version%/config.json"
 set "config_file=config.yml"
-
+set "account=1233456"
+set "password="
 if "%txlib_version%"=="8.9.63" (
     echo { "server": { "host": "%host%", "port": %port% }, "key": "%key%", "reload_interval": 40, "protocol": { "qua": "V1_AND_SQ_8.9.63_4194_YYB_D", "version": "8.9.63", "code": "4194" }, "unidbg": { "dynarmic": false, "unicorn": true, "debug": false } } > "%json_file%"
 )
-
 if "%txlib_version%"=="8.9.68" (
     echo { "server": { "host": "%host%", "port": %port% }, "key": "%key%", "reload_interval": 40, "protocol": { "qua": "V1_AND_SQ_8.9.68_4218_HDBM_T", "version": "8.9.68", "code": "4218" }, "unidbg": { "dynarmic": false, "unicorn": true, "debug": false } } > "%json_file%"
 )
-
 if "%txlib_version%" neq "8.9.63" (
   if "%txlib_version%" neq "8.9.68" (
-    echo 警告: 错误的 txlib_version！请输入正确的协议版本号！
+    echo Warning: Wrong txlib_ Version! Please enter the correct protocol version number!
     timeout 10
     exit /b
   )
 )
-
 if exist "%config_file%" (
   lib\sed.exe -i "/# sign-server:/d" "%config_file%"
   lib\sed.exe -i "s/sign-server:.*/sign-server: http:\/\/%host%:%port%/g; s/key:.*/key: %key%/g" "%config_file%"
 ) else (
-  echo 当前与go-cqhttp分离运行，请自行修改签名服务器地址及KEY .
+  echo Run separately from go-cqhttp. Please enter the sign-server address and KEY in go-cqhttp config.
 )
 
+findstr /C:"uin: 1233456" "%config_file%" 2>nul >nul
+if %errorlevel% equ 0 (
+    set /p "account=Account uin: "
+    set /p "password=Password: "
+    echo Your uin:!account! password:!password!
+    lib\sed.exe -i "s/uin: 1233456/uin: !account!/g; s/password: ''/password: '!password!'/g" "%config_file%"
+    echo Account and password saved!
+) else (
+    echo The config file already contains account information.
+)
 
 echo -----------------------------------------------------------
 echo Qsign API:http://%host%:%port%
@@ -40,8 +48,5 @@ echo KEY=%key%
 echo Qsign_version:%ver%
 echo TXlib_version:%txlib_version% 
 echo -----------------------------------------------------------
-
-
-
 timeout /t 3 > nul
 bin\unidbg-fetch-qsign --basePath=%library%%txlib_version%
