78791a951fbbbe8cdf3baeb5c1364d79f8e5fb8c
贰狐RH <58497504+rhwong@users.noreply.github.com>
2023-10-20 17:56:21 +0800

    update v1.1.9-f4

---
+++ README.md
@@ -0,0 +1,50 @@
+<div align="center">
+    <img alt="OOOK" src="https://olivos.onekey.ren/img/logo.png"/>
+
+# unidbg-fetch-qsign-onekey
+
+
+本项目用于一键启动[fuqiuluo/unidbg-fetch-qsign](https://github.com/fuqiuluo/unidbg-fetch-qsign)
+
+请自行将qsign内容文件和go-cqhttp放到同级别文件夹，脚本会自行读取<br>
+
+<img src="https://img.shields.io/github/issues/rhwong/unidbg-fetch-qsign-onekey"> <img src="https://img.shields.io/github/forks/rhwong/unidbg-fetch-qsign-onekey"> 
+<img src="https://img.shields.io/github/stars/rhwong/unidbg-fetch-qsign-onekey"> <img src="https://img.shields.io/badge/Version-1.1.9fix3-blue">
+<img src="https://img.shields.io/github/downloads/rhwong/unidbg-fetch-qsign-onekey/total">
+
+
+<img src="https://img.shields.io/badge/Windows-x64-red?style=flat-square&logo=Windows"> <img src="https://img.shields.io/badge/Windows-x86-red?style=flat-square&logo=Windows"> 
+
+</div>
+<!-- projectInfo end -->
+
+## ✨安装说明
+
+请直接点击 `Code - Download ZIP` 下载本项目，直接解压缩到任意不包含中文和空格的路径中。
+
+然后将qsign内容文件和go-cqhttp放到同级别文件夹，脚本会自行读取。
+
+## ✨使用方法
+
+### 如何使用go-cqhttp联动运行
+
+1. 首先，运行 `go-cqhttp.bat`，按照提示生成 `config.yml` 。
+
+2. 双击 `Start_Qsign.bat` 启动qsign，按照提示依次键入 `txlib_version` 参数、设定`host`、`port`、`key`的值。 (👈如果不知道这些是干什么的，请直接依次按下Enter）
+
+3. 在启动完成后，qsign会告诉您当前运行api的地址，如果您没设定过host和key，那么默认是 `http://127.0.0.1:13579`。key已经在开始时设定过，如果您没设定，那么默认是 `1145141919810`。(👈这些东西会自动写入到 `config.yml` ）
+
+4. 如果脚本检测到 `config.yml` 尚未设定账号和密码，那么会提示你输入账号(Account uin)和密码(password)，输入后会同步进 `config.yml` 。
+
+5. 如果日后需要修改 `txlib_version` 的版本，您可以删除文件夹根目录下的 `txlib_version.json` 来重新进入设定流程，也可以对此文件进行修改。
+
+
+## ✨免责声明
+
+- 本仓库代码仅供学习参考，用于自动化程序的启动，并无任何侵权内容。
+
+
+## Stargazers over time
+
+[![Stargazers over time](https://starchart.cc/rhwong/unidbg-fetch-qsign-onekey.svg)](https://starchart.cc/rhwong/unidbg-fetch-qsign-onekey)
+
---
+++ Start_Qsign.bat
@@ -0,0 +1,188 @@
+@echo off
+title Qsign-Onekey by rhwong v1.1.9-fix2
+setlocal enabledelayedexpansion
+set JAVA_HOME=.\jre
+set "ver=1.1.9-fix2"
+set "library=txlib/"
+set "config_file=config.yml"
+set "account=1233456"
+set "password="
+if not exist "txlib_version.json" (
+	REM txlib_version_config_file does not exist.
+) else ( 
+for /F "delims=" %%D in ('lib\jq.exe -r ".txlib_version" txlib_version.json') do set "txlib_version=%%D" 
+)
+
+set "json_file=%library%%txlib_version%/config.json"
+
+echo " ______  ______  __  ______  __   __       ______  __   __  ______  __  __  ______  __  __    "
+echo "/\  __ \/\  ___\/\ \/\  ___\/\ \-.\ \     /\  __ \/\ \-.\ \/\  ___\/\ \/ / /\  ___\/\ \_\ \   "
+echo "\ \ \/\_\ \___  \ \ \ \ \__ \ \ \-.  \    \ \ \/\ \ \ \-.  \ \  __\\ \  _'-\ \  __\\ \____ \  "
+echo " \ \___\_\/\_____\ \_\ \_____\ \_\\ \_\    \ \_____\ \_\\ \_\ \_____\ \_\ \_\ \_____\/\_____\ "
+echo "  \/___/_/\/_____/\/_/\/_____/\/_/ \/_/     \/_____/\/_/ \/_/\/_____/\/_/\/_/\/_____/\/_____/ "
+                                                                                              
+if not exist "txlib_version.json" (
+  echo -------------------------------------------------------------------------------------------------
+  echo unidbg-fetch-qsign-onekey Ver.%ver%
+  echo txlib_version_config_file does not exist.
+  echo Please enter an option to save. 
+  echo If you press enter directly, save the default values.
+  echo -------------------------------------------------------------------------------------------------
+  set /p "txlib_version=txlib_version(optional:3.5.1/3.5.2/8.9.58/8.9.63/8.9.68/8.9.70(default)/8.9.71/8.9.73/8.9.80): "
+       if "!txlib_version!"=="" (
+	   set "txlib_version=8.9.70"
+       )  
+  set "json_file=%library%!txlib_version!/config.json"
+  
+  set /p "host=host(default:127.0.0.1): "
+      if "!host!"=="" (
+      set "host=127.0.0.1"
+      )
+  set /p "port=port(default:13579): "
+      if "!port!"=="" (
+      set "port=13579"
+      )
+  set /p "key=key(default:1145141919810): "
+      if "!key!"=="" (
+      set "key=1145141919810"
+      )
+
+  if "!txlib_version!"=="3.5.1" (
+    echo { "server": { "host": "!host!", "port": !port! }, "share_token": false, "count": 5, "key": "!key!", "auto_register": true, "protocol": { "package_name": "com.tencent.tim", "qua": "V1_AND_SQ_8.3.9_351_TIM_D", "version": "3.5.1", "code": "1298" }, "unidbg": { "dynarmic": false, "unicorn": true, "kvm": false, "debug": false } } > "!json_file!"
+	  echo {"txlib_version": "3.5.1"} > txlib_version.json
+	)
+
+  if "!txlib_version!"=="3.5.2" (
+    echo { "server": { "host": "!host!", "port": !port! }, "share_token": false, "count": 5, "key": "!key!", "auto_register": true, "protocol": { "package_name": "com.tencent.tim", "qua": "V1_AND_SQ_8.3.9_352_TIM_D", "version": "3.5.2", "code": "1308" }, "unidbg": { "dynarmic": false, "unicorn": true, "kvm": false, "debug": false } } > "!json_file!"
+	  echo {"txlib_version": "3.5.2"} > txlib_version.json
+	)
+
+  if "!txlib_version!"=="8.9.58" (
+    echo { "server": { "host": "!host!", "port": !port! }, "share_token": false, "count": 5, "key": "!key!", "auto_register": true, "protocol": { "package_name": "com.tencent.mobileqq", "qua": "V1_AND_SQ_8.9.58_4106_YYB_D", "version": "8.9.58", "code": "4106" }, "unidbg": { "dynarmic": false, "unicorn": true, "kvm": false, "debug": false } } > "!json_file!"
+	  echo {"txlib_version": "8.9.58"} > txlib_version.json
+	)
+
+  if "!txlib_version!"=="8.9.63" (
+    echo { "server": { "host": "!host!", "port": !port! }, "share_token": false, "count": 5, "key": "!key!", "auto_register": true, "protocol": { "package_name": "com.tencent.mobileqq", "qua": "V1_AND_SQ_8.9.63_4194_YYB_D", "version": "8.9.63", "code": "4194" }, "unidbg": { "dynarmic": false, "unicorn": true, "kvm": false, "debug": false } } > "!json_file!"
+	  echo {"txlib_version": "8.9.63"} > txlib_version.json
+	)
+
+  if "!txlib_version!"=="8.9.68" (
+	echo { "server": { "host": "!host!", "port": !port! }, "share_token": false, "count": 5, "key": "!key!", "auto_register": true, "protocol": { "package_name": "com.tencent.mobileqq", "qua": "V1_AND_SQ_8.9.68_4264_YYB_D", "version": "8.9.68", "code": "4264" }, "unidbg": { "dynarmic": false, "unicorn": true, "kvm": false, "debug": false } } > "!json_file!"
+	  echo {"txlib_version": "8.9.68"} > txlib_version.json
+    )
+	
+  if "!txlib_version!"=="8.9.70" (
+    echo { "server": { "host": "!host!", "port": !port! }, "share_token": false, "count": 5, "key": "!key!", "auto_register": true, "protocol": { "package_name": "com.tencent.mobileqq", "qua": "V1_AND_SQ_8.9.70_4330_YYB_D", "version": "8.9.70", "code": "4330" }, "unidbg": { "dynarmic": false, "unicorn": true, "kvm": false, "debug": false } } > "!json_file!"
+	  echo {"txlib_version": "8.9.70"} > txlib_version.json
+	)
+	
+  if "!txlib_version!"=="8.9.71" (
+    echo { "server": { "host": "!host!", "port": !port! }, "share_token": false, "count": 5, "key": "!key!", "auto_register": true, "protocol": { "package_name": "com.tencent.mobileqq", "qua": "V1_AND_SQ_8.9.71_4332_YYB_D", "version": "8.9.71", "code": "4332" }, "unidbg": { "dynarmic": false, "unicorn": true, "kvm": false, "debug": false } } > "!json_file!"
+	  echo {"txlib_version": "8.9.71"} > txlib_version.json
+	)
+
+  if "!txlib_version!"=="8.9.73" (
+    echo { "server": { "host": "!host!", "port": !port! }, "share_token": false, "count": 5, "key": "!key!", "auto_register": true, "protocol": { "package_name": "com.tencent.mobileqq", "qua": "V1_AND_SQ_8.9.73_4416_YYB_D", "version": "8.9.73", "code": "4416" }, "unidbg": { "dynarmic": false, "unicorn": true, "kvm": false, "debug": false } } > "!json_file!"
+	  echo {"txlib_version": "8.9.73"} > txlib_version.json
+	)
+
+  if "!txlib_version!"=="8.9.80" (
+    echo { "server": { "host": "!host!", "port": !port! }, "share_token": false, "count": 5, "key": "!key!", "auto_register": true, "protocol": { "package_name": "com.tencent.mobileqq", "qua": "V1_AND_SQ_8.9.80_4614_YYB_D", "version": "8.9.80", "code": "4614" }, "unidbg": { "dynarmic": false, "unicorn": true, "kvm": false, "debug": false } } > "!json_file!"
+	  echo {"txlib_version": "8.9.80"} > txlib_version.json
+	)
+
+
+  if "!txlib_version!" neq "8.9.58" (
+    if "!txlib_version!" neq "8.9.63" (
+        if "!txlib_version!" neq "8.9.68" (
+          if "!txlib_version!" neq "8.9.70" (
+            if "!txlib_version!" neq "8.9.71" (
+              if "!txlib_version!" neq "8.9.73" (
+                if "!txlib_version!" neq "3.5.1" (
+                  if "!txlib_version!" neq "3.5.2" (
+                    if "!txlib_version!" neq "8.9.80" (
+            echo Warning: Wrong txlib_ Version. The protocol must be 3.5.1/3.5.2/8.9.58/8.9.63/8.9.68/8.9.70/8.9.73/8.9.80
+            timeout 10
+            exit /b
+                    )
+                  )
+                )
+              )
+            )
+          )
+        )
+      )
+  )
+) else (   
+  for /F "delims=" %%D in ('lib\jq.exe -r ".txlib_version" txlib_version.json') do set "txlib_version=%%D"
+  set "json_file=%library%!txlib_version!/config.json"
+  echo -------------------------------------------------------------------------------------------------
+  echo unidbg-fetch-qsign-onekey Ver.%ver%
+  echo txlib_Version is %txlib_version%
+  echo If you want to change txlib_version , please delete [txlib_version.json]!
+  echo -------------------------------------------------------------------------------------------------
+  for /F "delims=" %%A in ('lib\jq.exe -r ".server.host" %json_file%') do set "host=%%A"
+  for /F "delims=" %%B in ('lib\jq.exe -r ".server.port" %json_file%') do set "port=%%B"
+  for /F "delims=" %%C in ('lib\jq.exe -r ".key" %json_file%') do set "key=%%C"
+)
+
+set "targetPattern=*go-cqhttp*.exe"
+set "fileExists=0"
+
+for %%i in (%targetPattern%) do (
+    set "fileExists=1"
+)
+
+if %fileExists%==1 (
+  if exist "%config_file%" (
+    lib\sed.exe -i "s/url: 'http:\/\/127.0.0.1:8080'/Example-sign-server/; s/url: 'https:\/\/signserver.example.com'/Example-sign-server/" "%config_file%"
+    if "!host!"=="0.0.0.0" (
+      lib\sed.exe -i "0,/url: '.*'/s/url: '.*'/url: 'http:\/\/localhost:!port!'/; 0,/key: '.*'/s/key: '.*'/key: '!key!'/" "%config_file%"
+      ) else ( 
+      lib\sed.exe -i "0,/url: '.*'/s/url: '.*'/url: 'http:\/\/!host!:!port!'/; 0,/key: '.*'/s/key: '.*'/key: '!key!'/" "%config_file%"
+      )
+  ) else (
+    echo Can't find [config.yml]. If you forgot to generate it, please run [go-cqhttp.bat]
+  )
+      echo Sync protocol version to go-cqhttp data folder.
+      md data\versions
+      if "!txlib_version!" neq "3.5.1" (
+        if "!txlib_version!" neq "3.5.2" (
+          if "!txlib_version!" neq "8.9.71" (
+      copy txlib\!txlib_version!\android_pad.json data\versions\6.json
+      copy txlib\!txlib_version!\android_phone.json data\versions\1.json
+          )
+        )
+      )
+      if "!txlib_version!"=="8.9.71" (
+        echo Warning : This protocol just support ANDROID_PHONE now!!!!! Please changed device.json -> protocol=1
+        copy txlib\!txlib_version!\android_phone.json data\versions\1.json
+      )
+) else (
+  echo Run separately from go-cqhttp?
+  echo Please manually synchronize the protocol version.
+  echo And enter the Qsign API ADDRESS and KEY to other client.
+)  
+  findstr /C:"uin: 1233456" "%config_file%" 2>nul >nul
+  if %errorlevel% equ 0 (
+      set /p "account=Account uin: "
+      set /p "password=Password: "
+      echo -------------------------------------------------------------------------------------------------
+      echo Your uin:!account! password:!password!
+      lib\sed.exe -i "s/uin: 1233456/uin: !account!/g; s/password: ''/password: '!password!'/g; s/auto-refresh-token: false/auto-refresh-token: true/g" "%config_file%"
+      echo Account and password saved!
+  ) else (
+      echo -------------------------------------------------------------------------------------------------
+      echo The [config.yml] already contains account information or not exist.
+      echo Skip account settings.
+  )
+
+echo -------------------------------------------------------------------------------------------------
+echo Qsign API:http://!host!:!port!
+echo KEY=!key!
+echo Qsign_version:%ver%
+echo TXlib_version:%txlib_version% 
+echo -------------------------------------------------------------------------------------------------
+timeout /t 3 > nul
+bin\unidbg-fetch-qsign --basePath=%library%%txlib_version%
