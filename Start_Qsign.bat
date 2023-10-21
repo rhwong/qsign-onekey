9bb666bf6545e1fe1a8165be26d863cca161497e
贰狐 <58497504+rhwong@users.noreply.github.com>
2023-10-21 17:20:28 +0800

    pr form shiabox

--- Start_Qsign.bat
+++ Start_Qsign.bat
@@ -1,8 +1,8 @@
 @echo off
-title Qsign-Onekey by rhwong v1.1.9-fix2
+title Qsign-Onekey by rhwong shia v1.1.9-bitterest (2023-10-21)
 setlocal enabledelayedexpansion
 set JAVA_HOME=.\jre
-set "ver=1.1.9-fix2"
+set "ver=1.1.9-bitterest-2023-10-21"
 set "library=txlib/"
 set "config_file=config.yml"
 set "account=1233456"
@@ -12,14 +12,15 @@ if not exist "txlib_version.json" (
 ) else ( 
 for /F "delims=" %%D in ('lib\jq.exe -r ".txlib_version" txlib_version.json') do set "txlib_version=%%D" 
 )
-
 set "json_file=%library%%txlib_version%/config.json"
 
-echo " ______  ______  __  ______  __   __       ______  __   __  ______  __  __  ______  __  __    "
-echo "/\  __ \/\  ___\/\ \/\  ___\/\ \-.\ \     /\  __ \/\ \-.\ \/\  ___\/\ \/ / /\  ___\/\ \_\ \   "
-echo "\ \ \/\_\ \___  \ \ \ \ \__ \ \ \-.  \    \ \ \/\ \ \ \-.  \ \  __\\ \  _'-\ \  __\\ \____ \  "
-echo " \ \___\_\/\_____\ \_\ \_____\ \_\\ \_\    \ \_____\ \_\\ \_\ \_____\ \_\ \_\ \_____\/\_____\ "
-echo "  \/___/_/\/_____/\/_/\/_____/\/_/ \/_/     \/_____/\/_/ \/_/\/_____/\/_/\/_/\/_____/\/_____/ "
+echo -------------------------------------------------------------------------------------------------
+echo * ______  ______  __  ______  __   __       ______  __   __  ______  __  __  ______  __  __    *
+echo */\  __ \/\  ___\/\ \/\  ___\/\ \-.\ \     /\  __ \/\ \-.\ \/\  ___\/\ \/ / /\  ___\/\ \_\ \   *
+echo *\ \ \/\_\ \___  \ \ \ \ \__ \ \ \-.  \    \ \ \/\ \ \ \-.  \ \  __\\ \  _'-\ \  __\\ \____ \  *
+echo * \ \___\_\/\_____\ \_\ \_____\ \_\\ \_\    \ \_____\ \_\\ \_\ \_____\ \_\ \_\ \_____\/\_____\ *
+echo *  \/___/_/\/_____/\/_/\/_____/\/_/ \/_/     \/_____/\/_/ \/_/\/_____/\/_/\/_/\/_____/\/_____/ *
+echo -------------------------------------------------------------------------------------------------
                                                                                               
 if not exist "txlib_version.json" (
   echo -------------------------------------------------------------------------------------------------
@@ -28,9 +29,9 @@ if not exist "txlib_version.json" (
   echo Please enter an option to save. 
   echo If you press enter directly, save the default values.
   echo -------------------------------------------------------------------------------------------------
-  set /p "txlib_version=txlib_version(optional:3.5.1/3.5.2/8.9.58/8.9.63/8.9.68/8.9.70(default)/8.9.71/8.9.73/8.9.80): "
+  set /p "txlib_version=txlib_version(default:8.9.80): "
        if "!txlib_version!"=="" (
-	   set "txlib_version=8.9.70"
+	   set "txlib_version=8.9.80"
        )  
   set "json_file=%library%!txlib_version!/config.json"
   
@@ -47,73 +48,24 @@ if not exist "txlib_version.json" (
       set "key=1145141919810"
       )
 
-  if "!txlib_version!"=="3.5.1" (
-    echo { "server": { "host": "!host!", "port": !port! }, "share_token": false, "count": 5, "key": "!key!", "auto_register": true, "protocol": { "package_name": "com.tencent.tim", "qua": "V1_AND_SQ_8.3.9_351_TIM_D", "version": "3.5.1", "code": "1298" }, "unidbg": { "dynarmic": false, "unicorn": true, "kvm": false, "debug": false } } > "!json_file!"
-	  echo {"txlib_version": "3.5.1"} > txlib_version.json
-	)
-
-  if "!txlib_version!"=="3.5.2" (
-    echo { "server": { "host": "!host!", "port": !port! }, "share_token": false, "count": 5, "key": "!key!", "auto_register": true, "protocol": { "package_name": "com.tencent.tim", "qua": "V1_AND_SQ_8.3.9_352_TIM_D", "version": "3.5.2", "code": "1308" }, "unidbg": { "dynarmic": false, "unicorn": true, "kvm": false, "debug": false } } > "!json_file!"
-	  echo {"txlib_version": "3.5.2"} > txlib_version.json
-	)
-
-  if "!txlib_version!"=="8.9.58" (
-    echo { "server": { "host": "!host!", "port": !port! }, "share_token": false, "count": 5, "key": "!key!", "auto_register": true, "protocol": { "package_name": "com.tencent.mobileqq", "qua": "V1_AND_SQ_8.9.58_4106_YYB_D", "version": "8.9.58", "code": "4106" }, "unidbg": { "dynarmic": false, "unicorn": true, "kvm": false, "debug": false } } > "!json_file!"
-	  echo {"txlib_version": "8.9.58"} > txlib_version.json
-	)
-
-  if "!txlib_version!"=="8.9.63" (
-    echo { "server": { "host": "!host!", "port": !port! }, "share_token": false, "count": 5, "key": "!key!", "auto_register": true, "protocol": { "package_name": "com.tencent.mobileqq", "qua": "V1_AND_SQ_8.9.63_4194_YYB_D", "version": "8.9.63", "code": "4194" }, "unidbg": { "dynarmic": false, "unicorn": true, "kvm": false, "debug": false } } > "!json_file!"
-	  echo {"txlib_version": "8.9.63"} > txlib_version.json
-	)
-
-  if "!txlib_version!"=="8.9.68" (
-	echo { "server": { "host": "!host!", "port": !port! }, "share_token": false, "count": 5, "key": "!key!", "auto_register": true, "protocol": { "package_name": "com.tencent.mobileqq", "qua": "V1_AND_SQ_8.9.68_4264_YYB_D", "version": "8.9.68", "code": "4264" }, "unidbg": { "dynarmic": false, "unicorn": true, "kvm": false, "debug": false } } > "!json_file!"
-	  echo {"txlib_version": "8.9.68"} > txlib_version.json
-    )
-	
-  if "!txlib_version!"=="8.9.70" (
-    echo { "server": { "host": "!host!", "port": !port! }, "share_token": false, "count": 5, "key": "!key!", "auto_register": true, "protocol": { "package_name": "com.tencent.mobileqq", "qua": "V1_AND_SQ_8.9.70_4330_YYB_D", "version": "8.9.70", "code": "4330" }, "unidbg": { "dynarmic": false, "unicorn": true, "kvm": false, "debug": false } } > "!json_file!"
-	  echo {"txlib_version": "8.9.70"} > txlib_version.json
-	)
-	
-  if "!txlib_version!"=="8.9.71" (
-    echo { "server": { "host": "!host!", "port": !port! }, "share_token": false, "count": 5, "key": "!key!", "auto_register": true, "protocol": { "package_name": "com.tencent.mobileqq", "qua": "V1_AND_SQ_8.9.71_4332_YYB_D", "version": "8.9.71", "code": "4332" }, "unidbg": { "dynarmic": false, "unicorn": true, "kvm": false, "debug": false } } > "!json_file!"
-	  echo {"txlib_version": "8.9.71"} > txlib_version.json
-	)
-
-  if "!txlib_version!"=="8.9.73" (
-    echo { "server": { "host": "!host!", "port": !port! }, "share_token": false, "count": 5, "key": "!key!", "auto_register": true, "protocol": { "package_name": "com.tencent.mobileqq", "qua": "V1_AND_SQ_8.9.73_4416_YYB_D", "version": "8.9.73", "code": "4416" }, "unidbg": { "dynarmic": false, "unicorn": true, "kvm": false, "debug": false } } > "!json_file!"
-	  echo {"txlib_version": "8.9.73"} > txlib_version.json
-	)
-
-  if "!txlib_version!"=="8.9.80" (
-    echo { "server": { "host": "!host!", "port": !port! }, "share_token": false, "count": 5, "key": "!key!", "auto_register": true, "protocol": { "package_name": "com.tencent.mobileqq", "qua": "V1_AND_SQ_8.9.80_4614_YYB_D", "version": "8.9.80", "code": "4614" }, "unidbg": { "dynarmic": false, "unicorn": true, "kvm": false, "debug": false } } > "!json_file!"
-	  echo {"txlib_version": "8.9.80"} > txlib_version.json
-	)
-
-
-  if "!txlib_version!" neq "8.9.58" (
-    if "!txlib_version!" neq "8.9.63" (
-        if "!txlib_version!" neq "8.9.68" (
-          if "!txlib_version!" neq "8.9.70" (
-            if "!txlib_version!" neq "8.9.71" (
-              if "!txlib_version!" neq "8.9.73" (
-                if "!txlib_version!" neq "3.5.1" (
-                  if "!txlib_version!" neq "3.5.2" (
-                    if "!txlib_version!" neq "8.9.80" (
-            echo Warning: Wrong txlib_ Version. The protocol must be 3.5.1/3.5.2/8.9.58/8.9.63/8.9.68/8.9.70/8.9.73/8.9.80
-            timeout 10
-            exit /b
-                    )
-                  )
-                )
-              )
-            )
-          )
-        )
-      )
+if not exist "txlib\!txlib_version!\" (
+  echo -------------------------------------------------------------------------------------------------
+      echo Warning: Wrong txlib_ Version. 
+      echo Please check the "txlib" folder!
+      echo The following are supported txlib versions:
+      dir txlib /b /ad
+  echo -------------------------------------------------------------------------------------------------
+      timeout 10
+      exit /b
+  ) else (
+      for /F "delims=" %%P in ('lib\jq.exe -r ".protocol.package_name" !json_file!') do set "p_package_name=%%P"
+      for /F "delims=" %%Q in ('lib\jq.exe -r ".protocol.qua" !json_file!') do set "p_qua=%%Q"
+      for /F "delims=" %%V in ('lib\jq.exe -r ".protocol.version" !json_file!') do set "p_version=%%V"
+      for /F "delims=" %%O in ('lib\jq.exe -r ".protocol.code" !json_file!') do set "p_code=%%O"
+        echo { "server": { "host": "!host!", "port": !port! }, "share_token": false, "count": 5, "key": "!key!", "auto_register": true, "protocol": { "package_name": "!p_package_name!", "qua": "!p_qua!", "version": "!p_version!", "code": "!p_code!" }, "unidbg": { "dynarmic": false, "unicorn": true, "kvm": false, "debug": false } } > "!json_file!"
+        echo {"txlib_version": "!txlib_version!"} > txlib_version.json
   )
+
 ) else (   
   for /F "delims=" %%D in ('lib\jq.exe -r ".txlib_version" txlib_version.json') do set "txlib_version=%%D"
   set "json_file=%library%!txlib_version!/config.json"
@@ -154,10 +106,6 @@ if %fileExists%==1 (
       copy txlib\!txlib_version!\android_phone.json data\versions\1.json
           )
         )
-      )
-      if "!txlib_version!"=="8.9.71" (
-        echo Warning : This protocol just support ANDROID_PHONE now!!!!! Please changed device.json -> protocol=1
-        copy txlib\!txlib_version!\android_phone.json data\versions\1.json
       )
 ) else (
   echo Run separately from go-cqhttp?
