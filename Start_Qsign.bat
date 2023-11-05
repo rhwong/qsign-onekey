899b4bdf28e6c589e86310294f6f08b188af6b22
SilverDragon <73411104+kagangtuya-star@users.noreply.github.com>
2023-11-05 15:58:54 +0800

    fix:bug

--- Start_Qsign.bat
+++ Start_Qsign.bat
@@ -149,7 +149,7 @@ if %errorlevel% equ 0 (
     if defined pid (
       tasklist /fi "PID eq !pid!" | findstr /i "!pid!" >nul
         if %errorlevel% equ 0 (
-          askkill /F /PID !pid!))
+          taskkill /F /PID !pid!))
     start "Qsign-Onekey v1.2.3.1-Core" cmd /c "bin\unidbg-fetch-qsign --basePath=%library%%txlib_version%"
     timeout /t 15 /nobreak >nul
     for /f "tokens=5" %%A in ('netstat -ano ^| findstr ":!port!.*LISTENING"') do (
